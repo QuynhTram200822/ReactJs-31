2f0ba05ea0c1f83b80026463f0de5a9d2929e307
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2024-11-28 15:25:15 +0700

    Upload ReactJs-31

--- src/components/PageA/PageA.js
+++ src/components/PageA/PageA.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./PageA.css";
 
+import Img1 from '../../public/assets/images/1.jpg';
+// import Img2 from './../../../public/asesets/images/2.jpg';
+
+
 function Dashboard() {
   document.title = "Page A";
 
   return (
     <div className="App ">
-      <h2>This is Page A</h2>
+      <div  className="Img-container ">
+      <img src={Img1} alt="Loading..." />
+      </div>
+      <div  className="Img-container ">
+      <img src="/assets/images/2.jpg" alt="Loading..." />
+      </div>
     </div>
   );
 }
